bd3512db169c73689aba3d785c80cf120ce83106
xcy <2698064198@qq.com>
2019-04-16 17:08:35 +0800

    添加站点管理Cpu、Gq

--- shop1/zjyj/admin.py
+++ shop1/zjyj/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Cpu,Gq
 
-# Register your models here.
+admin.site.register(Cpu)
+admin.site.register(Gq)
+
--- shop1/zjyj/models.py
+++ shop1/zjyj/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Cpu(models.Model):
+    cname = models.CharField(max_length=255)
+    cbrand = models.CharField(max_length=255)
+    cseries = models.CharField(max_length=255)
+    ccore_num = models.CharField(max_length=255)
+    cmodel = models.CharField(max_length=255)
+    cprice = models.IntegerField()
+    # cimage = models.ImageField()
+
+    def __str__(self):
+        return self.cname
+
+
+class Gq(models.Model):
+    gname = models.CharField(max_length=255)
+    gbrand = models.CharField(max_length=255)
+    gmodel = models.CharField(max_length=255)
+    gcolor = models.CharField(max_length=255)
+    gcache = models.CharField(max_length=255)
+    ginterface = models.CharField(max_length=255)
+    gtype = models.CharField(max_length=255)
+    gsize = models.CharField(max_length=255)
+    gprice = models.IntegerField()
+
+    def __str__(self):
+        return self.gname
+
