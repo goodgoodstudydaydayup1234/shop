6a8b63166df531018dbb94ed3e112dc6b08dafe0
xcy <2698064198@qq.com>
2019-04-17 19:52:19 +0800

    首页、列表页、详情页传入cpu数据

--- shop1/zjyj/urls.py
+++ shop1/zjyj/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.index),
+    url(r'^$', views.index),
     url(r'^list/$', views.list),
+    url(r'^detail/(\d+)/$', views.detail),
 ]
 
--- shop1/zjyj/views.py
+++ shop1/zjyj/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Cpu, Gq
 
 
 def index(request):
-    return HttpResponse('首页')
+    # return HttpResponse('首页')
+    return render(request, 'shop1/index.html', {'username': 'xcy'})
 
 
 def list(request):
-    return HttpResponse('列表页')
+    cpulist = Cpu.objects.all()
+    return render(request, 'shop1/list.html', {'cpulist': cpulist})
+    # return HttpResponse('列表页')
 
+
+def detail(request, id):
+    cpu = Cpu.objects.get(pk=id)
+    return render(request, 'shop1/detail.html', {'cpu': cpu})
+    # return HttpResponse('详情页'+str(id))
