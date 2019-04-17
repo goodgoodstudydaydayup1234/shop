064a9ea304818fdc6d068417f4ceb488ecbf6570
xcy <2698064198@qq.com>
2019-04-17 21:13:38 +0800

    去除模板硬编辑

--- shop1/shop1/urls.py
+++ shop1/shop1/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('zjyj.urls'))
+    url(r'^', include('zjyj.urls', namespace='shop1'))
 ]
--- shop1/zjyj/urls.py
+++ shop1/zjyj/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'shop1'
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^list/$', views.list),
-    url(r'^detail/(\d+)/$', views.detail),
+    url(r'^$', views.index, name='index'),
+    url(r'^shop1list/$', views.list, name='list'),
+    url(r'^shop1detail/(\d+)/$', views.detail, name='detail'),
 ]
 
