310fc3fb6c0eb9efc89e4a041831c8588364f285
xcy <2698064198@qq.com>
2019-04-17 13:53:43 +0800

    ‘首页’和‘列表页’

--- shop1/zjyj/admin.py
+++ shop1/zjyj/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Cpu,Gq
 
-admin.site.register(Cpu)
-admin.site.register(Gq)
+
+class CpuAdmin(admin.ModelAdmin):
+    list_display = ['cname', 'cbrand', 'cmodel', 'cseries', 'ccore_num', 'cprice']
+    list_filter = ['cbrand', 'ccore_num']
+    search_fields = ['cbrand', 'ccore_num']
+    list_per_page = 3
+
+
+admin.site.register(Cpu, CpuAdmin)
+
+
+class GqAdmin(admin.ModelAdmin):
+    list_display = ['gname', 'gbrand', 'gmodel', 'gcolor', 'gcache', 'ginterface', 'gtype', 'gsize', 'gprice']
+    list_filter = ['gname']
+    search_fields = ['gname']
+    list_per_page = 3
+
+
+admin.site.register(Gq, GqAdmin)
 
